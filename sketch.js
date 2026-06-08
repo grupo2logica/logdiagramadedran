9ac1f9d1e35b46d7b3275acc5647cbfbd5219803
grupo2logica <davismat1506@gmail.com>
2026-06-08 06:21:22 -0300

    sketch.js

--- sketch.js
+++ sketch.js
@@ -2389,9 +2389,4 @@ function atualizarCamadasPorProposicoesSimples() {
 
 } 
 
-// Adicione isso no final do seu arquivo sketch.js
-function touchMoved() {
-  // Retornar true permite o comportamento de rolagem nativo do navegador
-  return true; 
-}
 
